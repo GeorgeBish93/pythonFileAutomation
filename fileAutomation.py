96307c2a08f3faf5b8a43deabeb8a3c229c8dbfd
GeorgeBish93 <George.bisharat93@gmail.com>
2023-03-20 11:13:25 +0200

    add payment by gender method

--- fileAutomation.py
+++ fileAutomation.py
@@ -1,5 +1,5 @@
 import openpyxl as xl
-from openpyxl.chart import BarChart,Reference
+from openpyxl.chart import BarChart,PieChart,Reference
 
 
 # function that gets file src and return a workbook object 'wb' from the loaded file
@@ -56,7 +56,7 @@ def customers_payment_method(filename,wb):
 
     #for lop that run on rows
     for row in range(2,sheet.max_row +1):
-        cell = sheet.cell(row,13)
+        cell = sheet.cell(row, 13)
 
         if cell.value == "Ewallet":
             ewallet += 1
@@ -68,13 +68,25 @@ def customers_payment_method(filename,wb):
     wb.create_sheet('PaymentMethod')
     new_sheet = wb['PaymentMethod']
     data = (
-        ("Payment Method","Cash", "Ewallet" , "Credit Card"),
-        ("Total Customers",cash,ewallet,card)
+        ("", "Cash", "Credit Card", "Ewallet"),
+        ("Total Customers", cash,card, ewallet )
     )
     for i in data:
         new_sheet.append(i)
 
-    wb.save(filename)
+    #create the chart and the references
+    chart = PieChart()
+    data = Reference(new_sheet, min_col=2, max_col=4, min_row=2, max_row=2)
+    labels = Reference(new_sheet, min_col=2, max_col=4, min_row=1)
+
+    #set the title, add the data to the chart and set the categories
+    chart.title = 'Customer Payment Method Distribution'
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(labels)
+
+    #add the chart and save the workbook as a new file
+    new_sheet.add_chart(chart, 'F5')
+    wb.save("filename.xlsx")
 
 def branch_customers(filename,wb):
     sheet = wb['Worksheet']
@@ -173,3 +185,59 @@ def net_income(filename,wb):
         new_sheet.append(i)
 
     wb.save(filename)
+
+    def payment_method_by_gender(filename, wb):
+        sheet = wb['Worksheet']
+        cashF = 0
+        cashM = 0
+        cardF = 0
+        cardM = 0
+        ewalletF = 0
+        ewalletM = 0
+
+        # for lop that run on rows
+        for row in range(2, sheet.max_row + 1):
+            genderCell = sheet.cell(row, 5)
+            paymentCell = sheet.cell(row, 13)
+
+            if paymentCell.value == "Ewallet":
+                if genderCell.value == "Female":
+                    ewalletF += 1
+                if genderCell.value == "Male":
+                    ewalletM += 1
+            if paymentCell.value == "Cash":
+                if genderCell.value == "Female":
+                    cashF += 1
+                if genderCell.value == "Male":
+                    cardM += 1
+            if paymentCell.value == "Credit Card":
+                if genderCell.value == "Female":
+                    cardF += 1
+                if genderCell.value == "Male":
+                    cardM += 1
+
+        cashTotal = cashM+cashF
+        percentageCashF = (cashF/(cashTotal))*100
+        percentageCashM = (cashM/(cashTotal))*100
+
+        cardTotal = cardF+cardM
+        percentageCardF = (cardF/(cardTotal))*100
+        percentageCardM = (cardM/(cardTotal))*100
+
+        eWalletTotal = ewalletM + ewalletF
+        percentageWalletF = (ewalletF / (eWalletTotal)) * 100
+        percentageWalletM = (ewalletM / (eWalletTotal)) * 100
+
+        wb.create_sheet('PaymentMethodByGender')
+        new_sheet = wb['PaymentMethodByGender']
+        data = (
+            ("", "Cash", "Credit Card", "Ewallet"),
+            ("Male", percentageCashM, percentageCardM, percentageWalletM),
+            ("Female", percentageCashF, percentageCardF, percentageWalletF)
+
+        )
+        for i in data:
+            new_sheet.append(i)
+
+        wb.save("filenam1.xlsx")
+
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ filename = 'sales.xlsx'
 wb = fa.load_workbook(filename)
 
 # call data automation funcions
-cs = fa.customers_type(filename,wb)
-pm = fa.customers_payment_method(filename,wb)
-customersNumberByBranch = fa.branch_customers(filename,wb)
-categorySales = fa.category_sales(filename,wb)
-netIncome = fa.net_income(filename,wb)
+
+paymentsByGender = fa.payment_method_by_gender(filename,wb)
+
+#cs = fa.customers_type(filename,wb)
+#pm = fa.customers_payment_method(filename,wb)
+#customersNumberByBranch = fa.branch_customers(filename,wb)
+#categorySales = fa.category_sales(filename,wb)
+#netIncome = fa.net_income(filename,wb)
